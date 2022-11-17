19af6d0630b15860a22efd9608086db59e537580
MustafaBasar471 <mustafabasar471@gmail.com>
2022-11-17 14:11:45 +0300

    .

--- reactstrapblog/src/pages/Dashboard.jsx
+++ reactstrapblog/src/pages/Dashboard.jsx
@@ -7,11 +7,41 @@ const Dashboard = () => {
     <Layout>
       <p className="fs-2 text-center mt-5">Dashboard</p>
       <div className="container d-flex flex-wrap gap-3 justify-content-center">
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
+        <div
+          style={{
+            width: "20rem",
+          }}
+        >
+          <BlogCard />
+        </div>
+        <div
+          style={{
+            width: "20rem",
+          }}
+        >
+          <BlogCard />
+        </div>
+        <div
+          style={{
+            width: "20rem",
+          }}
+        >
+          <BlogCard />
+        </div>
+        <div
+          style={{
+            width: "20rem",
+          }}
+        >
+          <BlogCard />
+        </div>
+        <div
+          style={{
+            width: "20rem",
+          }}
+        >
+          <BlogCard />
+        </div>
       </div>
     </Layout>
   );
